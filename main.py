867d1f4dbe707468ffa877e6a807fc01271934a0
Michael Bauer <da1da2da3ofme@gmail.com>
2016-11-28 16:05:25 +1100

    Don't want any actual keys to be visible

--- main.py
+++ main.py
@@ -10,12 +10,9 @@ from beaker.middleware import SessionMiddleware
 import alquery
 import qbuilder
 import pyalveo
+import settings
 import csv,json
 from io import BytesIO
-
-client_id = "608e3be7af68740374960833c4c905e6e6271b9378116bcb47b4f8d6aaf16c07"
-client_secret = "b316c0d1cdb01ec710964bc944c84747e6986e02bcb70540eb23138f8d14c339"
-redirect_url = "http://10.126.98.255:8000/oauth/callback"
 
 client = None
 
---
+++ settings.py
@@ -0,0 +1,10 @@
+'''
+Created on 28 Nov 2016
+
+@author: Michael
+'''
+
+#These are naturally examples and the keys will not work
+client_id = "608e3be7af68740374960833c4c905e6e6271b9378116bcb47b4f8d6aaf16c07"
+client_secret = "b316c0d1cdb01ec710964bc944c84747e6986e02bcb70540eb23138f8d14c339"
+redirect_url = "http://10.126.98.255:8000/oauth/callback"
