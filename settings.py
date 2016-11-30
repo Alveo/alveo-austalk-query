6abc0b262fe0d1de24ae9f448b82672da7202942
Michael Bauer <da1da2da3ofme@gmail.com>
2016-11-30 13:21:22 +1100

    Added default settings setting

--- settings.py
+++ settings.py
@@ -8,3 +8,4 @@ Created on 28 Nov 2016
 client_id = "608e3be7af68740374960833c4c905e6e6271b9378116bcb47b4f8d6aaf16c07"
 client_secret = "b316c0d1cdb01ec710964bc944c84747e6986e02bcb70540eb23138f8d14c339"
 redirect_url = "http://10.126.98.255:8000/oauth/callback"
+BASE_URL = 'https://app.alveo.edu.au/' #Normal Server
