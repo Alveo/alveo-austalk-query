7059b2311d0021a7d3f698ce19aa4750eef553a2
Michael Bauer <da1da2da3ofme@gmail.com>
2017-02-16 16:42:31 +1100

    fix settings

--- settings.py
+++ settings.py
@@ -4,14 +4,14 @@ Created on 28 Nov 2016
 @author: Michael
 '''
 
-redirect_url = "http://10.126.105.240:8000/oauth/callback"
+redirect_url = "https://10.126.105.240:8000/oauth/callback"
 
 #Real Server
 #BASE_URL = 'https://app.alveo.edu.au/' #Normal Server
 
 #Staging Server
 BASE_URL = 'https://alveo-staging.sol1.net/'
-client_id = "A55ad0231142d3e5ca75e897a03e95277fb6479e200a473a02c85fb7e7cb6656"
+client_id = "a55ad0231142d3e5ca75e897a03e95277fb6479e200a473a02c85fb7e7cb6656"
 client_secret = "d77d12411d7e18dee5f28172795391969eb727a407b0552561236e29217b9ab6"
 
 #Karls HTTP test server
