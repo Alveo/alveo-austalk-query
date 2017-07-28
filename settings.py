5eaeaeab1ec0ca00cb3f9c121d9a17474fbc141a
Michael Bauer <da1da2da3ofme@gmail.com>
2017-07-28 18:57:41 +1000

    standard settings file to read environment vars

---
+++ settings.py
@@ -0,0 +1,11 @@
+'''
+Created on 28 Nov 2016
+
+@author: Michael
+'''
+
+import os
+redirect_url = os.environ['REDIRECT_URL']
+BASE_URL = os.environ['APP_URL']
+client_id = os.environ['CLIENT_ID']
+client_secret = os.environ['CLIENT_SECRET']
