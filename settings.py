714798f8082477a070458cf7a7038f253e280def
Michael Bauer <da1da2da3ofme@gmail.com>
2017-10-12 14:50:27 +1100

    More flexible settings file

--- settings.py
+++ settings.py
@@ -5,8 +5,14 @@ Created on 28 Nov 2016
 '''
 
 import os
-redirect_url = os.environ['REDIRECT_URL']
-BASE_URL = os.environ['APP_URL']
-client_id = os.environ['CLIENT_ID']
-client_secret = os.environ['CLIENT_SECRET']
-log_file = os.environ['LOG_FILE']
+redirect_url = os.environ.get('REDIRECT_URL',None)
+BASE_URL = os.environ.get('APP_URL','https://app.alveo.edu.au')
+client_id = os.environ.get('CLIENT_ID',None)
+client_secret = os.environ.get('CLIENT_SECRET',None)
+log_file = os.environ.get('LOG_FILE','storage/log.csv')
+try:
+    max_log_size = int(os.environ.get('MAX_LOG_SIZE',5*1024*1024))
+except ValueError:
+    #Incase environment variable isn't a valid integer
+    #Default 5Mb
+    max_log_size = 5*1024*1024
